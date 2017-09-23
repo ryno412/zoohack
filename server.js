128f6c64c933d42beb546cac96f8161fc744e863
r-ford <r_ford@apple.com>
2017-09-23 00:54:29 -0700

    msg response

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const accountSid = 'AC05e5e36cdd8805d91483a43ffdba1ca3'; // Your Account SID fro
 const authToken = 'deaf596b59bdc360252d0782a69b3b94';   // Your Auth Token from www.twilio.com/console
 
 const twilio = require('twilio');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const client = new twilio(accountSid, authToken);
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -15,12 +16,48 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
+
+function respond(message) {
+    var twiml = new MessagingResponse();
+    twiml.message(message);
+    response.type('text/xml');
+    response.send(twiml.toString());
+}
+
 app.get('/', (req, res)=>{
     res.send('Welcome to the zoo zoo chat');
 })
 
+app.post('/message', (req, res)=>{
+    const phone = req.body.From;
+    const input = req.body.Body;
+    console.log("*******")
+    console.log(res.body);
+    console.log("*******")
+
+    const twiml = new MessagingResponse();
+    twiml.message(message);
+    res.type('text/xml');
+    res.send(twiml.toString());
+
+    // res.send({
+    //     status: 200,
+    //     phone,
+    //     msg: input
+    // })
+});
+
+
+
 app.get('/hello', (req, res) =>{
-    res.send('ok')
+    client.messages.create({
+    body: 'Hello from Node',
+    to: '+17605225669',  // Text this number
+    from: '+17603003698' // From a valid Twilio number
+}).then(message => {
+        console.log(message);
+        res.send('ok')
+    })
 })
 
 app.listen(port, ()=>{
@@ -28,11 +65,7 @@ app.listen(port, ()=>{
 })
 
 
-// client.messages.create({
-//     body: 'Hello from Node',
-//     to: '+17605225669',  // Text this number
-//     from: '+17603003698' // From a valid Twilio number
-// }).then((message) => console.log(message.sid));
+
 
 
 
