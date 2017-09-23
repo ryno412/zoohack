f85864265d0be1bb2a756e121bf6393a84c592f2
Sean Nam <namsangwoo1@gmail.com>
2017-09-23 15:36:26 -0700

    Receive MMS messages and extra media

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const accountSid = provess.env.TW_API;
-const authToken = process.env.TW_KEY;
+const accountSid = "ACfbec6cae01cb4afb173e268bd5501f5f";
+const authToken = "932725c460539deadb0154bdb594bf68";
 
 const twilio = require('twilio');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
@@ -8,6 +8,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const serveStatic = require('serve-static');
 const compression = require('compression');
+const extName = require('ext-name');
+const path = require('path');
+const urlUtil = require('url');
+
+var ExifImage = require('exif').ExifImage;
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -19,6 +25,7 @@ app.use(serveStatic(`${__dirname}/dist/client`, { index: ['index.html', 'index.h
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
+var images = []
 
 function respond(message) {
     var twiml = new MessagingResponse();
@@ -34,6 +41,35 @@ const chat = [
     'Lets make your first bird report. Describe the birds colors'
 ]
 
+function getRecentImages() {
+    return images;
+}
+
+function clearRecentImages() {
+    images = [];
+}
+
+function fetchRecentImages(req, res) {
+    res.status(200).send(getRecentImages());
+    clearRecentImages();
+}
+
+function getExif(file) {
+    console.log("getExif")
+    try {
+        new ExifImage({ image : file }, function (error, exifData) {
+        if (error)
+            console.log('Error: ' + error.message);
+        else
+            console.log('exifData');
+            console.log(exifData);
+
+        });
+      } catch (error) {
+        console.log('Error: ' + error.message);
+      }
+}
+
 app.post('/message', (req, res)=>{
     const phone = req.body.From;
     let input = req.body.Body;
@@ -47,6 +83,25 @@ app.post('/message', (req, res)=>{
     const twiml = new MessagingResponse();
     let msg = chat[0];
     if (input == 'yes') msg = chat[1];
+
+
+    const { body } = req;
+    const { NumMedia, From: SenderNumber, MessageSid } = body;
+    let saveOperations = [];
+    const mediaItems = [];
+
+    for (var i = 0; i < NumMedia; i++) {  // eslint-disable-line
+      const mediaUrl = body[`MediaUrl${i}`];
+      const contentType = body[`MediaContentType${i}`];
+      const extension = extName.mime(contentType)[0].ext;
+      const mediaSid = path.basename(urlUtil.parse(mediaUrl).pathname);
+      const filename = `${mediaSid}.${extension}`;
+
+      mediaItems.push({ mediaSid, MessageSid, mediaUrl, filename });
+      //saveOperations = mediaItems.map(mediaItem => SaveMedia(mediaItem));
+    }
+
+    clearRecentImages()
 
 
     twiml.message(msg);
@@ -70,8 +125,16 @@ app.get('/health', (req, res)=>{
     res.send('ok');
 })
 
+app.get('/exif', (req, res)=>{
+    // getExif('src/IMG_2709.jpg');
+    // getExif('src/IMG_2737.jpg');
+    // getExif('src/flower.jpg');
+    getExif('src/IMG_2739.jpg');
+    getExif('src/IMG_2739 2.jpg');
+})
+
 app.get('*', (req, res) => {
-    res.sendfile(`${__dirname}/dist/client/index.html`);
+    res.sendFile(`${__dirname}/dist/client/index.html`);
 });
 
 app.listen(port, ()=>{
